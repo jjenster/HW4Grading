a79c8907a4d28dea84e4128ec977c639763762ee
Linan Qiu <linanqiu@gmail.com>
2016-04-09 20:57:45 -0400

    reorganized

--- grader/src/KBestCounter.java
+++ grader/src/KBestCounter.java
@@ -1,42 +1,60 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
-public class KBestCounter<T extends Comparable<? super T>> {
+public class KBestCounter<T extends Comparable<T>> {
 
-	int k;
-	private PriorityQueue<T> prQueue;
+  PriorityQueue<T> heap;
+  int k;
 
-	public KBestCounter(int k) {
-		this.k = k;
-		prQueue = new PriorityQueue<>(k);
-	}
+  public KBestCounter(int k) {
+    this.k = k;
+    heap = new PriorityQueue<>(k);
+  }
 
-	public void count(T x) {
-		if (prQueue.size() < k) {
-			prQueue.add(x);
-		} else {
-			if (prQueue.peek().compareTo(x) <= 0) {
-				prQueue.poll();
-				prQueue.add(x);
-			}
-		}
-	}
+  public void count(T x) {
+    if (heap.size() < k) {
+      heap.offer(x);
+    } else {
+      if (heap.peek().compareTo(x) < 0) {
+        heap.poll();
+        heap.offer(x);
+      }
+    }
+  }
 
-	public List<T> kbest() {
-		List<T> sorted = new LinkedList<>();
-		ArrayList<T> temp = new ArrayList<>(prQueue.size());
+  public List<T> kbest() {
+    LinkedList<T> result = new LinkedList<>();
+    PriorityQueue<T> newHeap = new PriorityQueue<T>();
+    while (!heap.isEmpty()) {
+      T next = heap.remove();
+      result.addFirst(next);
+      newHeap.add(next);
+    }
+    heap = newHeap;
+    return result;
+  }
 
-		while (prQueue.size() > 0) {
-			T value = prQueue.poll();
-			sorted.add(0, value);
-			temp.add(value);
-		}
-		for (int i = 0; i < temp.size(); i++) {
-			prQueue.add(temp.get(i));
-		}
-		return sorted;
-	}
-
+  public static void main(String[] args) {
+    KBestCounter<Integer> kbest = new KBestCounter<>(3);
+    kbest.count(1);
+    kbest.count(4);
+    for (Integer i : kbest.kbest()) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    kbest.count(3);
+    kbest.count(2);
+    kbest.count(5);
+    for (Integer i : kbest.kbest()) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    kbest.count(1);
+    kbest.count(3);
+    kbest.count(10);
+    for (Integer i : kbest.kbest()) {
+      System.out.print(i + " ");
+    }
+  }
 }
