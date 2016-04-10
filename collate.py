eea2359b5a0ee93f69b85b2866ed042399abd768
Linan Qiu <linanqiu@gmail.com>
2016-04-10 18:11:49 -0400

    collate

--- collate.py
+++ collate.py
@@ -1,4 +1,32 @@
 import glob
+import os
+import csv
 
-for student_ in glob.glob('submissions_all/*'):
-  print grade_file
+section_points_max = [2, 3, 5, 3]
+
+data = {}
+
+unis = [os.path.basename(student_folder).split()[0] for student_folder in glob.glob('submissions_all/*')]
+grade_files = [os.path.join(student_folder, 'grade.txt') for student_folder in glob.glob('submissions_all/*')]
+
+for uni, grade_file in zip(unis, grade_files):
+  data[uni] = {'uni': uni}
+
+  if os.path.isfile(grade_file):
+    with open(grade_file, 'r') as grades_txt:
+      csv_reader = csv.reader(grades_txt, delimiter=':')
+      grades = list(csv_reader)
+      if len(grades) == 0:
+        data[uni]['grades'] = 0
+        data[uni]['comments'] = 'grade.txt is empty. please check.'
+      else:
+        data[uni]['grades'] = 25
+  else:
+    data[uni]['grades'] = 0
+    data[uni]['comments'] = 'grade.txt is not present. please check.'
+
+with open('grades-all.csv', 'w') as csv_file:
+  writer = csv.DictWriter(csv_file, fieldnames=['uni', 'grades', 'comments'])
+  writer.writeheader()
+  for value in data.values():
+    writer.writerow(value)
